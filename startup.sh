abed5ba4338eeab40eb875d8c944fe09fb54fc96
mannk <khacman98@gmail.com>
2023-11-13 16:12:44 +0700

    update

--- startup.sh
+++ startup.sh
@@ -6,8 +6,8 @@
 HOME='/home/mannk'
 
 [[ ! -f ${HOME}/.vnc/passwd ]] && {
-  mkdir ${HOME}/.vnc
-  x11vnc -storepasswd ${vncpasswd} ${HOME}/.vnc/passwd
+  sudo mkdir ${HOME}/.vnc
+  sudo x11vnc -storepasswd ${vncpasswd} ${HOME}/.vnc/passwd
 }
 
 sudo supervisord -c ${HOME}/supervisord.conf
