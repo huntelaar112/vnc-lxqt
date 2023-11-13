bbdb5500abcb940a6000a101b4de7447dfa2b14b
mannk <khacman98@gmail.com>
2023-11-13 20:52:31 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+conname="${1}"
+vncpasswd="${2}"
+
 docker network create -o "com.docker.network.bridge.name"=ocrnetwork --subnet=172.172.0.0/16 ocrnetwork
-docker rm -f manpc
-docker run -idt --name manpc --hostname manpc --network ocrnetwork --ip 172.172.0.101 -p 8083:5900 -e vncpasswd="hanoi123" mannk98/vnc-lxqt
+docker rm -f "${conname}"
+docker run -idt --name "${conname}" --hostname "${conname}" --network ocrnetwork --ip 172.172.0.101 -p 5900:5900 -e vncpasswd="${vncpasswd}" mannk98/vnc-lxqt
